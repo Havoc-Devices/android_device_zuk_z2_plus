9864da394cd5bcf3817a91741d847222aefd32ff
Nikhil Gohil <nikhil.gohil3@gmail.com>
2021-10-19 10:01:21 +0000

    z2_plus: add havoc official properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,3 +1,9 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.speaker.prot.enable=true
+
+# Havoc Official
+ro.havoc.device.name=Lenovo Zuk Z2 Plus
+ro.havoc.group.url=https://t.me/havoc_z2_plus
+ro.havoc.maintainer=Nikhil Gohil
+ro.havoc.maintainer.username=nikhilgohil3
